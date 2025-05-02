57b98a94c3600d75fcc1db96f221ad858cbcfe2f
nehhhhha6 <n0370525@gmail.com>
2025-05-02 17:17:20 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -53,7 +53,7 @@ export default function Home() {
               </svg>
               Twitter
             </a>
-            <a href="https://instagram.com/your-handle" className="social-link">
+            <a href="https://yourportfolio.com" className="social-link">
               <svg
                 width="20"
                 height="20"
@@ -61,9 +61,10 @@ export default function Home() {
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-1 16H6c-.55 0-1-.45-1-1V6c0-.55.45-1 1-1h12c.55 0 1 .45 1 1v12c0 .55-.45 1-1 1z" />
+                <path d="M8 11.5h8v1H8zm0-3h8v1H8zm0 6h4v1H8z" />
               </svg>
-              Instagram
+              Portfolio
             </a>
             <a
               href="https://linkedin.com/in/your-profile"
@@ -93,36 +94,6 @@ export default function Home() {
               Github
             </a>
           </div>
-        </div>
-      </section>
-
-      <section className="recent-posts">
-        <h2 className="section-title">RECENT POSTS</h2>
-        <div className="posts-grid">
-          <article className="post-card">
-            <div className="post-content">
-              <div className="post-meta">
-                <span>Oct 02, 2024</span>
-                <span>•</span>
-                <span>3 min</span>
-                <span>•</span>
-                <span>Thinking</span>
-              </div>
-              <h3 className="post-title">
-                <Link href="/blog/black-and-white-thinking">
-                  On Black and White Thinking
-                </Link>
-              </h3>
-              <p className="post-excerpt">
-                We often focus on the &quot;right&quot; way to do things, but
-                just as important, is the &quot;wrong&quot; way to go about
-                things. I&apos;ve recently become much more aware of an
-                anti-pattern that I keep running up against...
-              </p>
-            </div>
-          </article>
-
-          {/* You can add more post cards here */}
         </div>
       </section>
     </main>
